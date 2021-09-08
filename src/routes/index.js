72972d46d3053e748c53758aa83dab74d268640a
giraffeb <rwpark07@gmail.com>
2021-09-08 23:17:56 +0900

    init the-master-server

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ...require('./blogRoute'),
+  ...require('./commentRoute'),
+  ...require('./userRoute'),
+  ...require('./masterRoute'),
+  ...require('./serviceRouter'),
+  ...require('./reviewRouter'),
+};
