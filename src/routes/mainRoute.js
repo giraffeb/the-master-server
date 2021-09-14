77a235cc87b495e5feaf5bad05af9073a497a571
Jason <eros322@gmail.com>
2021-09-14 21:19:21 +0700

    Main API update

--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -15,9 +15,8 @@ categoryRoute.get("/", async(req, res) => {
 
         console.log('dataLength', dataLength);
 
-        var list = await Master.aggregate([
+        var popularList = await Master.aggregate([
             {$group: {_id:"$category",total:{$sum:1}}},
-            {$sort: {total: -1}},
             {$limit: dataLength},            
             {$lookup:
                 {
@@ -26,17 +25,23 @@ categoryRoute.get("/", async(req, res) => {
                    localField: "_id",                // field in the QuotStep collection
                    as: "category",                   
                 }
-            }
+            },
+            {$unwind: {
+              path: "$category"
+            }},
+            {$project: {_id:1, total: 1, categoryName: "$category.categoryName", imageUri: "$category.imageUri"}},
+            {$sort: {total: -1}}
         ]);
 
         
         var newList = await ServiceCategory.aggregate([
             {$sort: {releaseDate: -1, categoryName: 1}},
+            {$project: {_id:1, total: 1, categoryCode: 1, categoryName: 1, releaseDate: 1, imageUri : 1}},
             {$limit: dataLength}
         ]);
 
         return res.status(200).json({
-            popular: list,
+            popular: popularList,
             newList: newList
         });
 
